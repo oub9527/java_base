8830369013a1c57c0a116af950fb6013a35e0c0a
oub9527 <81718917@qq.com>
2019-05-22 16:26:12 +0800

    代理模式

---
+++ src/com/ob/base/designmode/proxypattern/SchoolGirl.java
@@ -0,0 +1,19 @@
+package com.ob.base.designmode.proxypattern;
+
+/**
+ * @Author: oubin
+ * @Date: 2019/5/22 15:25
+ * @Description:
+ */
+public class SchoolGirl {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
