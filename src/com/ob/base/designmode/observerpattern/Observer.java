d050872547243ea9b73b1524115b8b724bdb40be
oub9527 <81718917@qq.com>
2019-05-23 17:46:01 +0800

    观察者模式(发布-订阅)

---
+++ src/com/ob/base/designmode/observerpattern/Observer.java
@@ -0,0 +1,11 @@
+package com.ob.base.designmode.observerpattern;
+
+/**
+ * @Author: oubin
+ * @Date: 2019/5/23 09:47
+ * @Description:
+ */
+public abstract class Observer {
+
+    public abstract void update();
+}
