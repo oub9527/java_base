72ee50cb438ada96a2aa36601fa0b73346c095e6
oub9527 <81718917@qq.com>
2019-05-24 08:56:09 +0800

    单例模式

---
+++ src/com/ob/base/designmode/singletonpattern/Singleton.java
@@ -0,0 +1,26 @@
+package com.ob.base.designmode.singletonpattern;
+
+/**
+ * @Author: oubin
+ * @Date: 2019/5/24 08:50
+ * @Description:
+ */
+public class Singleton {
+
+    private static Singleton singleton;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance(){
+        if (null == singleton) {
+            synchronized (Singleton.class) {
+                if (null == singleton) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
